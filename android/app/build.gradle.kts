ddfa737f0dda4e103def932733f40f7dd2945979
c1c3ru <cicerosilva.ifce@gmail.com>
2026-08-29 11:39:05 -0300

    chore: downgrade project dependencies and Android build configuration to stable versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.c1c3ru.apostamegasena"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "28.2.13676358"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
